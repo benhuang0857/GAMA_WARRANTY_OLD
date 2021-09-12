f6b627c14b4c8bbe2ec79333a2aacf21b16abf82
benhuang0857 <benhuang0857@gmail.com>
2021-09-12 15:43:49 +0800

    fix share content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,9 @@
 <div class="container">
     <div class="row justify-content-center shadow_box">
         <img src="{{asset('/images/site/BIG_GAMA.png')}}" class="center" alt="">
-        <h3 style="text-align: center">歡迎進入GAMA推薦專區(Welcome to GAMA recommand area)</h3>
+        <h3 style="text-align: center">歡迎進入GAMA推薦專區</h3><br>
+        <h3 style="text-align: center">Welcome to GAMA recommand area</h3>
+
         <div style="width: 100%; padding:6px; background:rgb(245, 153, 32);color:white;text-align: center">LOGIN</div>
 
         <div class="col-md-8" style="margin: 0px">
