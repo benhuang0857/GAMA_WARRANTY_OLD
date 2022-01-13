6a5582c5dc11fbdb24c08ddd5f5bda12b7e84425
benhuang0857 <benhuang0857@gmail.com>
2022-01-13 20:24:41 +0800

    20220113 line login

--- config/line.php
+++ config/line.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'channel_id' => env('1656801322'),
-    'secret' => env('b39e5b6e8e7f88582c41c8d1034717a4'),
+    'channel_id' => env('LINE_CHANNEL_ID'),
+    'secret' => env('LINE_SECRET'),
     'authorize_base_url' => 'https://access.line.me/oauth2/v2.1/authorize',
     'get_token_url' => 'https://api.line.me/oauth2/v2.1/token',
     'get_user_profile_url' => 'https://api.line.me/v2/profile',
